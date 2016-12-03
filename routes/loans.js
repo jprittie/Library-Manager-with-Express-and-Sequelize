ebe2f2b34395c89d087727e64817ea750def1ba0
jprittie <eyewallop@gmail.com>
2016-12-03 10:40:48 -0500

    Adds overdue loans

--- routes/loans.js
+++ routes/loans.js
@@ -5,10 +5,37 @@ var Loan = require("../models").Loan;
 var Patron = require("../models").Patron;
 
 
-/* GET loans page. */
+/* GET loans page */
 router.get('/', function(req, res, next) {
   Loan.findAll({
     include: [{ all: true }]
+  }).then(function(loanlistings){
+    res.render('partials/loans', {
+      title: 'Loans',
+      loans: loanlistings
+    });
+  });
+});
+
+/* GET overdue loans */
+router.get('/overdue', function(req, res, next) {
+  Loan.findAll({
+    include: [{ all: true }],
+		where: { return_by: { $lt: new Date() }, returned_on: null }
+  }).then(function(loanlistings){
+    res.render('partials/loans', {
+      title: 'Loans',
+      loans: loanlistings
+    });
+  });
+});
+
+/* GET checked-out loans */
+// But what's the difference between all loans and checked-out loans?
+router.get('/checked_out', function(req, res, next) {
+  Loan.findAll({
+    include: [{ all: true }],
+  	where: { returned_on: null }
   }).then(function(loanlistings){
     res.render('partials/loans', {
       title: 'Loans',
