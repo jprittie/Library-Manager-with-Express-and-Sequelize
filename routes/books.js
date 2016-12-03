8f377c2df5e34ba454c84505dbbedc50d0577e31
jprittie <eyewallop@gmail.com>
2016-12-03 10:14:18 -0500

    Adds overdue & checked-out books

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
+var Loan = require("../models").Loan;
+var Patron = require("../models").Patron;
 
 /* GET books page. */
 router.get('/', function(req, res, next) {
   Book.findAll().then(function(booklistings){
     res.render('partials/books', {
-      title: 'Books', 
+      title: 'Books',
+      books: booklistings
+    });
+  });
+});
+
+//Overdue books aren't populating
+router.get('/overdue', function(req, res, next) {
+  Loan.findAll({
+    include: [{ model: Book}],
+    where: { return_by: { $lt: new Date() }, returned_on: null }
+  }).then(function(booklistings){
+    res.render('partials/books', {
+      title: 'Overdue Books',
+      books: booklistings
+    });
+  });
+});
+
+//Checked out books aren't populating
+router.get('/checked_out', function(req, res, next) {
+  Loan.findAll({
+    include: [{ model: Book}],
+    where: { returned_on: null }
+  }).then(function(booklistings){
+    res.render('partials/books', {
+      title: 'Overdue Books',
       books: booklistings
     });
   });
