22ad993c58b74f030af4a96db36f3d6d8377f379
jprittie <eyewallop@gmail.com>
2016-12-03 10:13:48 -0500

    Adds models

--- routes/loans.js
+++ routes/loans.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Book = require("../models").Book;
 var Loan = require("../models").Loan;
+var Patron = require("../models").Patron;
+
 
 /* GET loans page. */
 router.get('/', function(req, res, next) {
-  Loan.findAll().then(function(loanlistings){
+  Loan.findAll({
+    include: [{ all: true }]
+  }).then(function(loanlistings){
     res.render('partials/loans', {
       title: 'Loans',
       loans: loanlistings
--- routes/patrons.js
+++ routes/patrons.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var Book = require("../models").Book;
+var Loan = require("../models").Loan;
 var Patron = require("../models").Patron;
+
 
 /* GET patrons page. */
 router.get('/', function(req, res, next) {
